01513321114e1c0e8bb728d40d3db30620d9fb5e
Dineshkumaar07 <dineshkumaar2003@gmail.com>
2024-02-17 16:13:50 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,123 @@
+"use client";
 import Image from "next/image";
+import dynamic from "next/dynamic";
 
+import desktopSideImg from "../../images/imageD.svg";
+import mobileSideImg from "../../images/imageS.svg";
+
+import bigTick from "../../images/icon-success.svg";
+import { useMediaQuery } from "react-responsive";
+
+import listTickImg from "../../images/icon-list.svg";
+import { useState } from "react";
 export default function Home() {
+  const isSmallScreen = useMediaQuery({ maxWidth: 640 });
+  const isMediumScreen = useMediaQuery({ minWidth: 641 });
+
+  const liStlye = "flex gap-3";
+  const [page, setPage] = useState(1);
+  const [email, setEmail] = useState("");
+  const [ok, setOk] = useState(true);
+  function validate(email: string) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+  }
+  function emailValidate(event: any) {
+    event.preventDefault();
+    const email = event.target.value;
+    if (validate(email) && email !== "") {
+      setOk(true);
+      setEmail(email);
+    } else {
+      setOk(false);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="sm:flex h-screen w-full sm:justify-center sm:items-center text-[#242742] sm:bg-[#242742]">
+      <title>Newsletter-Subscription</title>
+      {page === 1 && (
+        <div className="flex flex-col-reverse sm:gap-9 sm:flex-row bg-white rounded-3xl sm:p-6  items-center ">
+          <div className="flex flex-col gap-4 sm:gap-7 sm:py-24 py-5 sm:px-9 px-5">
+            <h1 className="text-4xl sm:text-5xl font-bold">Stay updated!</h1>
+            <p>
+              Join 60,000+ product managers receiving monthly
+              <br /> updates on:
+            </p>
+            <ul className="flex flex-col gap-3">
+              <li className={liStlye}>
+                <Image src={listTickImg} alt="tick" />
+                <p>Product discovery and building what matters</p>{" "}
+              </li>
+              <li className={liStlye}>
+                <Image src={listTickImg} alt="tick" />
+                <p>Measuring to ensure updates are a success</p>
+              </li>
+              <li className={liStlye}>
+                <Image src={listTickImg} alt="tick" />
+                <p>And much more!</p>
+              </li>
+            </ul>
+            <div className="flex flex-col gap-3">
+              <div className="flex w-full justify-between">
+                <span className="text-sm font-semibold">Email address</span>
+                {!ok && (
+                  <span className="text-sm font-semibold text-[#ff6257]">
+                    valid email required
+                  </span>
+                )}
+              </div>
+              <input
+                onChange={emailValidate}
+                type="text"
+                placeholder="email@company.com"
+                className={
+                  ok
+                    ? "outline outline-2 outline-gray-300 px-9 py-2 rounded-md focus:outline-gray-500 "
+                    : "outline outline-2 outline-red-300 px-9 py-2 rounded-md focus:outline-red-300 bg-red-200 text-[#ff6257] "
+                }
+              />
+            </div>
+            <button
+              className="bg-[#242742] text-white px-9 py-3 rounded-md"
+              disabled={!ok}
+              onClick={() => {
+                console.log(email);
+                setPage(2);
+              }}
+            >
+              Subscribe to monthly newsletter{" "}
+            </button>
+          </div>
+          <div>
+            {isSmallScreen && <Image src={mobileSideImg} alt="Hi" />}
+
+            {isMediumScreen && <Image src={desktopSideImg} alt="Hi" />}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+      {page === 2 && (
+        <div className="sm:w-1/4 flex flex-col gap-5bg-white sm:px-14 px-9 bg-white sm:py-9 rounded-3xl justify-around">
+          <div className="flex flex-col gap-5 ">
+            <Image src={bigTick} alt="tick" height="50" />
+            <h1 className="font-bold text-4xl">Thanks for subscribing!</h1>
+            <p>
+              A confirmation email has been sent to {email}. Please open it and
+              click the button inside to confirm your subscription.
+            </p>
+          </div>
+          <button
+            className="bg-[#242742] text-white px-9 py-3 rounded-md mt-6"
+            onClick={() => {
+              setPage(1);
+              setEmail("");
+            }}
+          >
+            {" "}
+            Dismiss message
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
